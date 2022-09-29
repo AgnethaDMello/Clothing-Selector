13dbe385a1b6d66c71ed075e6d4fd53e266d5e80
AgnethaDMello <agnethad@nycstudents.net>
2022-09-28 22:38:40 -0400

    slayed

--- apcsp/src/App.vue
+++ apcsp/src/App.vue
@@ -5,14 +5,18 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300&display=swap" rel="stylesheet">
         <h1 id="header">Clothing Gallery!</h1>
-        <section @click="theme" class="btn" id="theme">Change Theme</section>
-        <div id="gallery" v-for="(item, index) in items"
+        <div id="whole">
+          <button id="btn1" class="btn1" @click="button1 = !button1">Hide Shirts</button>
+        <button id="btn2" class="btn2" @click="button2 = !button2">Hide Pants</button>
+        <div class="row">
+        <div class="column" v-if="button1">
+  <div v-for="(item, index) in items"
         :key="item.name"
             :name="item.name"
             :image="item.img"
             :desc="item.desc"
             :index="index">
-            <div id="column1" class="cards" v-if="item.shirt">
+            <div class="cards" v-if="item.shirt">
               <h2 class="shirtnames">
     {{ item.name }}
             </h2>
@@ -21,17 +25,35 @@
     {{ item.desc }}
             </h3>
           </div>
-  <div id="column2" class="cards" v-else>
-              <h2 class="pantnames">
+  <div class="hidden" v-else>
+            </div>
+  </div>
+</div>
+<div class="row">
+        <div id="pantcolumn" class="column" v-if="button2">
+  <div v-for="(item, index) in items"
+        :key="item.name"
+            :name="item.name"
+            :image="item.img"
+            :desc="item.desc"
+            :index="index">
+            <div class="cards" v-if="item.pant">
+              <h2 class="shirtnames">
     {{ item.name }}
             </h2>
-            <img class="pantimgs" :src="item.img"/>
+            <img class="shirtimgs" :src="item.img"/>
             <h3 class="light" >
     {{ item.desc }}
             </h3>
+          </div>
+  <div class="hidden" v-else>
+            </div>
   </div>
         </div>
-    </div>
+      </div>
+  </div>
+</div>
+</div>
   </body>
     </template>
 <script>
@@ -39,6 +61,8 @@ export default {
     name: "Home",
     data() {
     return {
+      button1: true,
+      button2: true,
       items: [
         {
             name: "Button Down",
@@ -90,87 +114,116 @@ export default {
         },
         {
             name: "Track Pant",
-            shirt: false,
+            pant: true,
             img: "https://underarmour.scene7.com/is/image/Underarmour/PS1357201-001_HF?rp=standard-0pad|pdpMainDesktop&scl=1&fmt=jpg&qlt=85&resMode=sharp2&cache=on,on&bgc=F0F0F0&wid=566&hei=708&size=566,708 ",
             desc: "A black track pant, comfortable and hugs your legs well while exercising.",
         },
         {
             name: "Sweatpants",
-            shirt: false,
+            pant: true,
             img: "https://media.gq.com/photos/6036b7eeb86c65eb745394a8/master/w_2000,h_1333,c_limit/Goodwear-fleece-sweatpants-with-side-pockets.jpg",
             desc: "A grey sweatpant, cozy and snug for being both lazy and active.",
         },
         {
             name: "Straight Pants",
-            shirt: false,
+            pant: true,
             img: "https://img.abercrombie.com/is/image/anf/KIC_156-2331-0775-410_prod1?policy=product-medium ",
             desc: "A tan straight legged pant, great for formal occassions.",
         },
         {
             name: "Flared",
-            shirt: false,
+            pant: true,
             img: "https://cdn-images.farfetch-contents.com/17/88/99/09/17889909_39807318_300.jpg",
             desc: "A black flared legging, modern and chic, plus they pair well with many shirts.",
         },
         {
             name: "Skinny Jean",
-            shirt: false,
+            pant: true,
             img: "https://i.pinimg.com/originals/98/12/ff/9812ff098d3e16dd61fad13db217c50f.jpg",
             desc: "A dark blue skinny jean, a continuity in the fashion world for many years.",
         },
         {
             name: "Wide Jean",
-            shirt: false,
+            pant: true,
             img: "https://media.companys.com/images/light-blue-elmagz-hw-wide-jeans.jpg?i=AP6cq-RN2Ag/428509&mw=610",
             desc: "A light blue wide jean, covers your leg shape and can become baggy if sized up.",
         },
         {
             name: "Distressed Jean",
-            shirt: false,
+            pant: true,
             img: "https://cdn-fnknc.nitrocdn.com/jwqHRGAzpUgGskUSHlppNQzwuXgXIKwg/assets/static/optimized/rev-c69cd9d/wp-content/uploads/2022/08/Amiri_BestRippedJeans.jpg",
             desc: "A washed out light blue jean with tears, a bold punk statement.",
         },
         {
             name: "Cargo Pant",
-            shirt: false,
+            pant: true,
             img: "https://admin.kaufmannstatic.com/Images/177940_seaweed_01-T20220713013313.jpg?i=177940_seaweed_01-T20220713013313.jpg&w=300&h=300&bgcolor=ffffff",
             desc: "Light grey-green cargo pants, practical for work and great for streetstyle.",
         },
       ],
-    };
-},
-methods: {
-  theme: function () {
-    if (document.body.classList.contains("light")) {
-        document.body.classList.add("dark");
-        document.body.classList.remove("light");
     }
-    else {
-        document.body.classList.add("light");
-        document.body.classList.remove("dark");
-    }
-},
+  },
+    mounted: function() {
+    this.buttonClick();
+  },
+  methods: {
+    buttonClick: function() {
+      const btn1 = document.getElementById('btn1');
+      // ✅ Toggle button text on click
+btn1.addEventListener('click', function handleClick() {
+  const initialText = 'Hide Shirts';
+
+  if (btn1.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn1.textContent = 'Show Shirts';
+  } else {
+    btn1.textContent = initialText;
+  }
+});
+      const btn2 = document.getElementById('btn2');
+      // ✅ Toggle button text on click
+btn2.addEventListener('click', function handleClick() {
+  const initialText = 'Hide Pants';
+
+  if (btn2.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn2.textContent = 'Show Pants';
+  } else {
+    btn2.textContent = initialText;
+  }
+});
+    },
 }
 }
 </script>
 
 <style scoped>
-  .btn,
-.btn:link,
-.btn:visited {
+  * {
+  box-sizing: border-box;
+}
+.column {
+  float: left;
+  width: 20rem;
+  margin-left: 22%;
+}
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+button,
+button:link,
+button:visited {
     text-transform: uppercase;
     text-decoration: none;
-    padding: 1rem 3rem;
+    padding: 1rem 2.5rem;
     display: inline-block;
-    margin-left: 39.5rem;
     margin-bottom: 2rem;
-    font-size: 1.5rem;
+    font-size: var(--h3);
     border: none;
     border-radius: 10rem;
     transition: all 0.2s;
     color: var(--cool);
 }
-.btn:hover {
+button:hover {
     transform: translateY(-2px);
     box-shadow: 0 10px 0.5rem rgba(0, 0, 0, 0.2);
 }    
@@ -189,26 +242,28 @@ img {
   margin-bottom: 5rem;
   width: 20rem;
 }
-#column1{
-  margin-left: 12rem;
+.btn1{
+  margin-left: 20rem;
 }
-#column2{
-  margin-left: 62rem;
-  margin-top: -275.5rem;
-  margin-bottom: 280.5rem;
+.btn2{
+  margin-left: 23rem;
 }
-#gallery{
-  display: flex;
+#whole{
+  display: inline;
+}
+#pantcolumn{
+  position: absolute;
+  margin-left: 54.5rem;
 }
 h2{
   text-align: center;
   margin-bottom:1rem;
-  color: var(--header);
+  color: var(--text);
 
 }
 h3{
   text-align: center;
   margin-top: 1rem;
-  color: var(--header);
+  color: var(--text);
 }
 </style>
